c7822d027cffc9f95d87bbe69438bb5c43728b16
Manohar Prabhu <manoharagent13@gmail.com>
2021-11-04 01:13:34 +0530

    use gulp to build and minify

--- build/cli.js
+++ build/cli.js
@@ -1,8 +1,2 @@
 #!/usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Game_1 = __importDefault(require("./Game"));
-new Game_1.default(20).start();
+"use strict";var __importDefault=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(exports,"__esModule",{value:!0});const Game_1=__importDefault(require("./Game"));new Game_1.default(20).start();
